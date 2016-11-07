1a8887bdb32e323283aed33f53535ff5155c0c7f
Malcolm Jack <inquisitorjax@gmail.com>
2016-11-07 23:52:52 +0200

    Add working UWP example

--- Samples.Syncfusion.XamarinForms.Pdf/Samples.Syncfusion.XamarinForms.Pdf.UWP/MainPage.xaml.cs
+++ Samples.Syncfusion.XamarinForms.Pdf/Samples.Syncfusion.XamarinForms.Pdf.UWP/MainPage.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Xamarin.Forms;
 
 namespace Samples.Syncfusion.XamarinForms.Pdf.UWP
 {
@@ -22,6 +9,8 @@ namespace Samples.Syncfusion.XamarinForms.Pdf.UWP
             this.InitializeComponent();
 
             LoadApplication(new Samples.Syncfusion.XamarinForms.Pdf.App());
+
+            DependencyService.Register<ISaveFileStreamCommand, UWPSaveFileStreamCommand>();
         }
     }
 }
---
+++ Samples.Syncfusion.XamarinForms.Pdf/Samples.Syncfusion.XamarinForms.Pdf.UWP/UWPSaveFileStreamCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Wibci.LogicCommand;
+using Windows.Storage;
+
+namespace Samples.Syncfusion.XamarinForms.Pdf.UWP
+{
+    public class UWPSaveFileStreamCommand : AsyncLogicCommand<SaveFileStreamContext, DeviceCommandResult>, ISaveFileStreamCommand
+    {
+        public override async Task<DeviceCommandResult> ExecuteAsync(SaveFileStreamContext request)
+        {
+            StorageFolder local = ApplicationData.Current.LocalFolder;
+
+            StorageFile outFile = await local.CreateFileAsync(request.FileName, CreationCollisionOption.ReplaceExisting);
+
+            using (Stream outStream = await outFile.OpenStreamForWriteAsync())
+            {
+                outStream.Write(request.Stream.ToArray(), 0, (int)request.Stream.Length);
+            }
+
+            if (request.LaunchFile)
+            {
+                await Windows.System.Launcher.LaunchFileAsync(outFile);
+            }
+
+            return new DeviceCommandResult();
+        }
+    }
+}
