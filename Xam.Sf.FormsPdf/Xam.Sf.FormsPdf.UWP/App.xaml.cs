865cbc985f472fd0fdb8285bfd6046ed0c06ecdd
Malcolm Jack <inquisitorjax@gmail.com>
2019-07-29 23:24:39 +0200

    UWP CropCircle

--- Xam.Sf.FormsPdf/Xam.Sf.FormsPdf.UWP/App.xaml.cs
+++ Xam.Sf.FormsPdf/Xam.Sf.FormsPdf.UWP/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Samples.Syncfusion.XamarinForms.Pdf;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Wibci.Xamarin.Images;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -17,85 +19,90 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Xam.Sf.FormsPdf.UWP
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : Application
-    {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
-        }
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : Application
+	{
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			this.InitializeComponent();
+			this.Suspending += OnSuspending;
+		}
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+
+			Frame rootFrame = Window.Current.Content as Frame;
+
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame();
+
+				rootFrame.NavigationFailed += OnNavigationFailed;
+
+				Xamarin.Forms.Forms.Init(e);
+
+				Xamarin.Forms.DependencyService.Register<ISaveFileStreamCommand, UWPSaveFileStreamCommand>();
+				Xamarin.Forms.DependencyService.Register<IResizeImageCommand, Wibci.Xamarin.Images.UWP.UWPResizeImageCommand>();
+				Xamarin.Forms.DependencyService.Register<IAnalyseImageCommand, Wibci.Xamarin.Images.UWP.UWPAnalyseImageCommand>();
+				Xamarin.Forms.DependencyService.Register<IImageUtility, WindowsImageUtility>();
 
 
-            Frame rootFrame = Window.Current.Content as Frame;
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					//TODO: Load state from previously suspended application
+				}
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+				// Place the frame in the current Window
+				Window.Current.Content = rootFrame;
+			}
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
+			if (rootFrame.Content == null)
+			{
+				// When the navigation stack isn't restored navigate to the first page,
+				// configuring the new page by passing required information as a navigation
+				// parameter
+				rootFrame.Navigate(typeof(MainPage), e.Arguments);
+			}
+			// Ensure the current window is active
+			Window.Current.Activate();
+		}
 
-                Xamarin.Forms.Forms.Init(e);
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
-            // Ensure the current window is active
-            Window.Current.Activate();
-        }
-
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
-    }
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+			//TODO: Save application state and stop any background activity
+			deferral.Complete();
+		}
+	}
 }
---
+++ Xam.Sf.FormsPdf/Xam.Sf.FormsPdf.UWP/WindowsImageUtility.cs
@@ -0,0 +1,79 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Shapes;
+using Size = SixLabors.Primitives.Size;
+using System.IO;
+
+namespace Xam.Sf.FormsPdf.UWP
+{
+	public class WindowsImageUtility : IImageUtility
+	{
+		public byte[] TransformIntoCircle(byte[] image)
+		{
+			//doc: https://github.com/SixLabors/Samples/blob/master/ImageSharp/AvatarWithRoundedCorner/Program.cs
+			byte[] retImageBytes;
+			using (var img = Image.Load(image))
+			{
+				var breadth = img.Width > img.Height ? img.Height : img.Width;
+				var cornerRadius = breadth / 2;
+				var size = new Size(breadth, breadth);
+				using (var cropped = img.Clone(x => WindowsImageUtility.ConvertToAvatar(x, size, cornerRadius)))
+				{
+					var memoryStream = new MemoryStream();
+					cropped.SaveAsPng(memoryStream);
+					retImageBytes = memoryStream.ToArray();
+				}
+
+			}
+
+			return retImageBytes;
+		}
+
+		private static IImageProcessingContext<Rgba32> ConvertToAvatar(IImageProcessingContext<Rgba32> processingContext, Size size, float cornerRadius)
+		{
+			return processingContext.Resize(new ResizeOptions
+			{
+				Size = size,
+				Mode = ResizeMode.Crop
+			}).Apply(i => ApplyRoundedCorners(i, cornerRadius));
+		}
+
+		public static void ApplyRoundedCorners(Image<Rgba32> img, float cornerRadius)
+		{
+			IPathCollection corners = BuildCorners(img.Width, img.Height, cornerRadius);
+
+			var graphicOptions = new GraphicsOptions(true)
+			{
+				AlphaCompositionMode = PixelAlphaCompositionMode.DestOut // enforces that any part of this shape that has color is punched out of the background
+			};
+			// mutating in here as we already have a cloned original
+			// use any color (not Transparent), so the corners will be clipped
+			img.Mutate(x => x.Fill(graphicOptions, Rgba32.LimeGreen, corners));
+		}
+
+		public static IPathCollection BuildCorners(int imageWidth, int imageHeight, float cornerRadius)
+		{
+			// first create a square
+			var rect = new RectangularPolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
+
+			// then cut out of the square a circle so we are left with a corner
+			IPath cornerTopLeft = rect.Clip(new EllipsePolygon(cornerRadius - 0.5f, cornerRadius - 0.5f, cornerRadius));
+
+			// corner is now a corner shape positions top left
+			//lets make 3 more positioned correctly, we can do that by translating the orgional artound the center of the image
+
+			float rightPos = imageWidth - cornerTopLeft.Bounds.Width + 1;
+			float bottomPos = imageHeight - cornerTopLeft.Bounds.Height + 1;
+
+			// move it across the width of the image - the width of the shape
+			IPath cornerTopRight = cornerTopLeft.RotateDegree(90).Translate(rightPos, 0);
+			IPath cornerBottomLeft = cornerTopLeft.RotateDegree(-90).Translate(0, bottomPos);
+			IPath cornerBottomRight = cornerTopLeft.RotateDegree(180).Translate(rightPos, bottomPos);
+
+			return new PathCollection(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight);
+		}
+	}
+
+}
+
